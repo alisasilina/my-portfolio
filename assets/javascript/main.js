c0ae768aa167038c732b926c8d706f43733d5d2d
Alisa Silina <alisasilina@gmail.com>
2020-04-10 22:02:59 +0300

    First commit (home, about, skills)

---
+++ assets/javascript/main.js
@@ -0,0 +1,74 @@
+// Preloader
+$(window).on('load', function (event) {
+    $('.preloader').delay(400).fadeOut(400);
+});
+
+// Navbar
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 200) nav.className = 'nav-container pullDown'; else nav.className = 'nav-container pullDown scroll';
+};
+
+// Zoom in on scroll
+AOS.init({
+  duration: 800, // values from 0 to 3000, with step 50ms
+});
+
+// Carousel (slider)
+jQuery(document).ready(function ($) {
+
+  var interval;
+  interval = setInterval(function () {
+    moveRight();
+  }, 4000);
+
+  $('._slider').mouseover(function(){
+    clearInterval(interval);
+  });
+
+  $('._slider').mouseleave(function(){
+    interval = setInterval(function () {
+      moveRight();
+      }, 4000);
+  });
+
+  var slideCount = $('._slider ul li').length;
+  var slideWidth = $('._slider ul li').width();
+  var slideHeight = $('._slider ul li').height();
+  var sliderUlWidth = slideCount * slideWidth;
+
+  $('._slider').css({ width: slideWidth, height: slideHeight });
+
+  $('._slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+    $('._slider ul li:last-child').prependTo('._slider ul');
+
+    function moveLeft() {
+        $('._slider ul').animate({
+            left: + slideWidth
+        }, 800, function () {
+            $('._slider ul li:last-child').prependTo('._slider ul');
+            $('._slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('._slider ul').animate({
+            left: - slideWidth
+        }, 800, function () {
+            $('._slider ul li:first-child').appendTo('._slider ul');
+            $('._slider ul').css('left', '');
+        });
+    };
+
+    $('._slider_prev').click(function () {
+        moveLeft();
+        return false;
+    });
+
+    $('._slider_next').click(function () {
+        moveRight();
+        return false;
+    });
+
+});
